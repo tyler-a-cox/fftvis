7a5002fa4a976bdfb45387ecc964259f8ce78b72
Tyler Cox <tyler.a.cox@berkeley.edu>
2024-02-14 09:08:56 -0800

    add 3d transformation

--- fftvis/cpu.py
+++ fftvis/cpu.py
@@ -247,15 +247,18 @@ def simulate_cpu(
     return vis
 
 
-def simulate_basis(
-    antpos: dict,
+def simulate_basis_gridded(
     freqs: np.ndarray,
     basis_comps: np.ndarray,
-    basis: np.ndarray,
-    beam,
-    ra: np.ndarray,
-    dec: np.ndarray,
+    crd_eq: np.ndarray,
+    eq2tops: np.ndarray,
     precision: int = 1,
+    polarized: bool = False,
+    use_redundancy: bool = True,
+    vectorize_times: bool = True,
+    check: bool = False,
+    accuracy: float = 1e-6,
+    ngrid: int = 256,
 ):
     """
     Simulate the sky using some basis as simulation
@@ -266,4 +269,168 @@ def simulate_basis(
         - 1: float32, complex64
         - 2: float64, complex128
     """
-    pass
+    # Check inputs are valid
+    if check:
+        nax, nfeeds, nants, ntimes = _validate_inputs(
+            precision, polarized, antpos, eq2tops, crd_eq, sources
+        )
+    else:
+        nax, nfeeds, ntimes, nfreqs = (
+            1,
+            1,
+            eq2tops.shape[0],
+            basis_comps.shape[-1],
+        )
+
+    if precision == 1:
+        real_dtype = np.float32
+        complex_dtype = np.complex64
+    else:
+        real_dtype = np.float64
+        complex_dtype = np.complex128
+
+    # Convert to correct precision
+    crd_eq = crd_eq.astype(real_dtype)
+    eq2tops = eq2tops.astype(real_dtype)
+    Isky = (0.5 * basis_comps).astype(complex_dtype)
+
+    # Zero arrays:
+    vis = np.full((ngrid, ngrid, ntimes, nfreqs), 0, dtype=complex_dtype)
+
+    # Loop over time samples
+    for ti, eq2top in enumerate(eq2tops):
+        # Convert to topocentric coordinates
+        tx, ty, tz = crd_top = np.dot(eq2top, crd_eq)
+
+        above_horizon = tz > 0
+        tx = tx[above_horizon]
+        ty = ty[above_horizon]
+
+        # Compute the beam sky product
+        i_sky = Isky[above_horizon]
+
+        # TODO: finufft2d1 is not vectorized over time
+        for ni in range(nfreqs):
+            vis[..., ti, ni] = finufft.nufft2d1(
+                2 * np.pi * ty,
+                2 * np.pi * tx,
+                np.ascontiguousarray(i_sky[:, ni]),
+                n_modes=ngrid,
+                modeord=0,
+                eps=accuracy,
+            )
+
+    return vis
+
+
+def simulate_basis(
+    antpos: dict,
+    eta: np.ndarray,
+    freqs: np.ndarray,
+    sources: np.ndarray,
+    crd_eq: np.ndarray,
+    eq2tops: np.ndarray,
+    precision: int = 1,
+    polarized: bool = False,
+    check: bool = False,
+    accuracy: float = 1e-6,
+    use_redundancy=True,
+):
+    """
+    Simulate the sky using some basis as simulation
+
+    precision : int, optional
+        Which precision level to use for floats and complex numbers
+        Allowed values:
+        - 1: float32, complex64
+        - 2: float64, complex128
+    """
+    # Check inputs are valid
+    if check:
+        nax, nfeeds, nants, ntimes = _validate_inputs(
+            precision, polarized, antpos, eq2tops, crd_eq, sources
+        )
+    else:
+        nax, nfeeds, ntimes, nfreqs = (
+            1,
+            1,
+            eq2tops.shape[0],
+            freqs.shape[-1],
+        )
+
+    if precision == 1:
+        real_dtype = np.float32
+        complex_dtype = np.complex64
+    else:
+        real_dtype = np.float64
+        complex_dtype = np.complex128
+
+    # Get the redundant - TODO handle this better
+    reds = get_pos_reds(antpos)
+    baselines = [red[0] for red in reds]
+    nbls = len(baselines)
+    nants = len(antpos)
+
+    # prepare beam
+    # beam = conversions.prepare_beam(beam)
+
+    if use_redundancy:
+        bl_to_red_map = {red[0]: np.array(red) for red in reds}
+
+    # Convert to correct precision
+    crd_eq = crd_eq.astype(real_dtype)
+    eq2tops = eq2tops.astype(real_dtype)
+    Isky = (0.5 * sources).astype(complex_dtype)
+
+    # Compute coordinates
+    blx, bly = np.array([antpos[bl[1]] - antpos[bl[0]] for bl in baselines])[
+        :, :2
+    ].T.astype(real_dtype)
+
+    # Zero arrays:
+    vis = np.full((nants, nants, ntimes, nfreqs), 0, dtype=complex_dtype)
+
+    # Loop over time samples
+    for ti, eq2top in enumerate(eq2tops):
+        # Convert to topocentric coordinates
+        tx, ty, tz = crd_top = np.dot(eq2top, crd_eq)
+        above_horizon = tz > 0
+        tx = tx[above_horizon]
+        ty = ty[above_horizon]
+
+        # Sky Coordinates
+        tX, _eta = np.meshgrid(tx, eta)
+        tY, _ = np.meshgrid(ty, eta)
+
+        # Baseline coordinates
+        U, tF = np.meshgrid(blx / speed_of_light, freqs)
+        V, _ = np.meshgrid(bly / speed_of_light, freqs)
+
+        # Simulate
+        _vis = finufft.nufft3d3(
+            2 * np.pi * np.ravel(tY),
+            2 * np.pi * np.ravel(tX),
+            np.ravel(_eta),
+            np.ravel(Isky[above_horizon]),
+            np.ravel(V * tF),
+            np.ravel(U * tF),
+            np.ravel(tF),
+            modeord=0,
+            eps=accuracy,
+        )
+        _vis.shape = (nfreqs, nbls)
+
+        if use_redundancy:
+            for bi, bls in enumerate(baselines):
+                np.add.at(
+                    vis,
+                    (bl_to_red_map[bls][:, 0], bl_to_red_map[bls][:, 1], ti),
+                    _vis[:, bi],
+                )
+                np.add.at(
+                    vis,
+                    (bl_to_red_map[bls][:, 1], bl_to_red_map[bls][:, 0], ti),
+                    _vis[:, bi].conj(),
+                )
+
+    return vis
