b9cd5fdaa4d72ce06aeccca84fb334741385008e
Tyler Cox <tyler.a.cox@berkeley.edu>
2024-03-20 16:29:53 -0700

    add skeleton code for sky, beam, and mc fitting

--- fftvis/optimize.py
+++ fftvis/optimize.py
@@ -6,26 +6,73 @@ import tqdm
 import jax
 from jax import numpy as jnp
 from .simulate import _FFT_simulator
+from jax._src.typing import Array
 
 
-def _compute_loss(labels: jnp.ndarray, predictions: jnp.ndarray) -> jnp.ndarray:
+#
+def _evaluate_input_model_parameters(
+    model_parameters: dict,
+    fit_mode: str,
+    freqs: Array,
+    times: Array,
+    ra: Array,
+    dec: Array,
+):
+    """
+    Evaluate the input parameters for different fit modes.
+    """
+    if fit_mode == "sky":
+        return model_parameters, freqs, times, ra, dec
+    elif fit_mode == "mutual_coupling":
+        return model_parameters, freqs, times, ra, dec
+    elif fit_mode == "beam":
+        return model_parameters, freqs, times, ra, dec
+    else:
+        raise ValueError(f"Fit mode {fit_mode} not recognized.")
+
+
+def _validate_sky_fit_inputs(beam: str, beam_diameter: float, beam_vals: Array):
+    """ """
+    pass
+
+
+def _compute_loss_sky_fit(
+    model_parameters: dict,
+    freqs: Array,
+    times: Array,
+    ra: Array,
+    dec: Array,
+    labels: Array,
+) -> jnp.ndarray:
     """
     Compute the loss between the labels and the predictions.
 
     Parameters:
     ----------
+    model_parameters : dict
+        The model parameters to fit.
+    freqs : jnp.ndarray
+        The frequencies of the data.
+    times : jnp.ndarray
+        The times of the data.
+    ra : jnp.ndarray
+        The right ascension of the source.
+    dec : jnp.ndarray
+        The declination of the source.
     labels : jnp.ndarray
-        The true values.
-    predictions : jnp.ndarray
-        The predicted values.
+        The data to fit.
 
     Returns:
     -------
     jnp.ndarray
         The loss.
     """
+    # Evaluate the sky model for a given set of parameters
+    sky_model = _evaluate_sky_model(model_parameters=model_parameters, freqs=freqs)
+
     # Initialize the model
-    model = _FFT_simulator(
+    predictions = _FFT_simulator(
+        sky_model=sky_model,
         times=times,
         freqs=freqs,
         ra=ra,
@@ -55,7 +102,7 @@ def _evaluate_sky_model(
     )
 
 
-def fit_sky(
+def _fit_sky(
     model_parameters: dict,
     optimizer: optax.GradientTransformation,
     data: jnp.ndarray,
@@ -63,6 +110,13 @@ def fit_sky(
     freqs: jnp.ndarray,
     ra: jnp.ndarray,
     dec: jnp.ndarray,
+    beam: str = "airy",
+    beam_diameter: float = 14.6,
+    beam_vals: jnp.ndarray = None,
+    beam_az: jnp.ndarray = None,
+    beam_za: jnp.ndarray = None,
+    diffuse_component: bool = False,
+    lmax: int = 50,
     nsteps: int = 100,
 ) -> tuple[dict, jnp.ndarray]:
     """
@@ -101,29 +155,105 @@ def fit_sky(
 
     @functools.partial(jax.vmap, in_axes=(None, 0, 0), axis_name="num_device")
     def parallelized_updates(model_parameters, coords, v):
+        """
+        Parallize data across devices to compute updates.
+        """
         pass
 
     @jax.jit
     def update(model_parameters):
-        """ """
+        """
+        Update the model parameters.
+        """
         pass
 
     for _ in tqdm.tqdm(range(nsteps)):
-        loss, grads = jax.value_and_grad(_compute_loss)(parameters, coords, v)
+        # TODO: Add support for parallelized updates
+        loss, grads = jax.value_and_grad(_compute_loss_sky_fit)(parameters, coords, v)
         updates, opt_state = optimizer.update(grads, opt_state, parameters)
         parameters = optax.apply_updates(parameters, updates)
         losses.append(loss)
 
     return parameters, jnp.array(losses)
 
-def fit_mutual_coupling():
+
+def _fit_mutual_coupling(
+    model_parameters: dict,
+    optimizer: optax.GradientTransformation,
+    data: jnp.ndarray,
+    times: jnp.ndarray,
+    freqs: jnp.ndarray,
+    sky_model: jnp.ndarray,
+    ra: jnp.ndarray,
+    dec: jnp.ndarray,
+    nsteps: int = 100,
+) -> tuple[dict, jnp.ndarray]:
     """
-    Fit the mutual coupling between antennas.
+    Skeleton function for fitting mutual coupling.
+
+    Parameters:
+    ----------
+    model_parameters : dict
+        A dictionary containing the model parameters.
+    optimizer : optax.GradientTransformation
+        The optimizer to use for the fitting.
+    data : jnp.ndarray
+        The data to fit. Data here has shape (ntimes, nfrequencies, nbaselines).
+    times : jnp.ndarray
+        The times of the data. Must match the number of times in data
+    freqs : jnp.ndarray
+        The frequencies of the data. Must match the number of frequencies in data
+    sky_model : jnp.ndarray
+        The sky model to use for the fitting.
+    ra : jnp.ndarray
+        Source positions in right ascension.
+    dec : jnp.ndarray
+        Source positions in declination.
+    nsteps : int
+        The number of steps to run the optimizer.
+
+    Returns:
+    -------
+    dict
+        The best fit model.
+    jnp.ndarray
+        The loss history.
     """
     pass
 
-def fit_beam():
+
+def _fit_beam(
+    model_parameters: dict,
+    optimizer: optax.GradientTransformation,
+    data: jnp.ndarray,
+    times: jnp.ndarray,
+    freqs: jnp.ndarray,
+    ra: jnp.ndarray,
+    dec: jnp.ndarray,
+    sky_model: jnp.ndarray,
+    nsteps: int = 100,
+) -> tuple[dict, jnp.ndarray]:
     """
-    Fit the beam pattern of the antennas.
+    Skeleton function for fitting the beam.
     """
-    pass
+    model_parameters = _evaluate_input_model_parameters(
+        model_parameters=model_parameters,
+        fit_mode="mutual_coupling",
+        freqs=freqs,
+        times=times,
+        ra=ra,
+        dec=dec,
+    )
+
+    # Initialize the model
+    opt_state = optimizer.init(model_parameters)
+    losses = []
+
+    for _ in tqdm.tqdm(range(nsteps)):
+        # TODO: Add support for parallelized updates
+        loss, grads = jax.value_and_grad(_compute_loss_sky_fit)(parameters, coords, v)
+        updates, opt_state = optimizer.update(grads, opt_state, parameters)
+        parameters = optax.apply_updates(parameters, updates)
+        losses.append(loss)
+
+    return parameters, jnp.array(losses)
