ebe0a8cdd053878e259ddf7439fb0c1308f5e119
Tyler Cox <tyler.a.cox@berkeley.edu>
2024-04-18 15:09:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fftvis",
-    version="0.0.3",
+    version="0.0.4",
     description="An FFT-based visibility simulator",
     author="Tyler Cox",
     author_email="tyler.a.cox@berkeley.edu",
