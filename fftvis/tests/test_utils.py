262dd72eda2d3676f169b5aa42eebcd0bca1ae8d
Tyler Cox <tyler.a.cox@berkeley.edu>
2024-08-23 11:37:22 -0700

    add simple test for rotation matrix

--- fftvis/tests/test_utils.py
+++ fftvis/tests/test_utils.py
@@ -1,6 +1,25 @@
 import pytest
+import numpy as np
+from fftvis import utils
 
+def test_get_plane_to_xy_rotation_matrix():
+    """
+    """
+    # Rotate the array to the xy-plane
+    x = np.linspace(0, 100, 100)
+    y = np.linspace(0, 100, 100)
+    z = 0.125 * x + 0.5 * y
+    antvecs = np.array([x, y, z]).T
 
-def test_get_pos_reds():
-    """ """
-    pass
+    rotation_matrix = utils.get_plane_to_xy_rotation_matrix(antvecs)
+    rm_antvecs = np.dot(rotation_matrix.T, antvecs.T)
+
+    # Check that all elements of the z-axis are close to zero
+    np.testing.assert_allclose(rm_antvecs[-1], 0, atol=1e-12)
+    
+    # Check that the lengths of the vectors are preserved
+    np.testing.assert_allclose(
+        np.linalg.norm(antvecs, axis=1), 
+        np.linalg.norm(rm_antvecs, axis=0), 
+        atol=1e-12
+    )
