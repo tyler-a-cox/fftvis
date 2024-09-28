30ebeb6b451860b9dc7717b8fa245b2cc652152a
Tyler Cox <tyler.a.cox@berkeley.edu>
2024-09-27 17:16:49 -0700

    update init to check for gpu first

--- fftvis/__init__.py
+++ fftvis/__init__.py
@@ -1,2 +1,13 @@
-from . import beams, utils, simulate
+try:
+    import cupy
+
+    HAVE_GPU = True
+except ImportError:
+    HAVE_GPU = False
+
+from . import cpu
 from .wrapper import simulate_vis
+from . import beams, utils, simulate
+
+if HAVE_GPU:
+    from . import gpu
