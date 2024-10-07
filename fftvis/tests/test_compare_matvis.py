5a80feeb3a1b37bae2660cb8353546c3830c6c63
Tyler Cox <tyler.a.cox@berkeley.edu>
2024-10-07 14:07:27 -0700

    consolidate tests

--- fftvis/tests/test_compare_matvis.py
+++ fftvis/tests/test_compare_matvis.py
@@ -8,7 +8,8 @@ from . import get_standard_sim_params
 @pytest.mark.parametrize('polarized', [False, True])
 @pytest.mark.parametrize('precision', [2,1])
 @pytest.mark.parametrize('use_analytic_beam', [True, False])
-def test_simulate(polarized: bool, precision: int, use_analytic_beam: bool):
+@pytest.mark.parametrize('tilt_array', [True, False])
+def test_simulate(polarized: bool, precision: int, use_analytic_beam: bool, tilt_array: bool):
 
     (
         ants,
@@ -22,6 +23,10 @@ def test_simulate(polarized: bool, precision: int, use_analytic_beam: bool):
         location,
     ) = get_standard_sim_params(use_analytic_beam, polarized)
     
+    if tilt_array:
+        # Tilt the array
+        ants = {ant: np.array(ants[ant]) + np.array([0, 0, ai * 5]) for ai, ant in enumerate(ants)}
+
     # Simulate with specified baselines
     sim_baselines = np.array([[0, 1]])#, [0, 2], [1, 2]])
 
@@ -60,52 +65,3 @@ def test_simulate(polarized: bool, precision: int, use_analytic_beam: bool):
             fvis[..., bi], mvis[:, :, bi], 
             atol=1e-5 if precision==2 else 1e-4
         )
-
-
-
-def test_simulate_non_coplanar():
-    # Simulation parameters
-    ntimes = 10
-    nfreqs = 5
-    nants = 3
-    nsrcs = 20
-
-    # Set random set
-    rng = np.random.default_rng(42)
-
-    # Define frequency and time range
-    freqs = np.linspace(100e6, 200e6, nfreqs)
-    lsts = np.linspace(0, np.pi, ntimes)
-
-    # Set up the antenna positions
-    antpos = {k: np.array([k * 10, 0, k]) for k in range(nants)}
-    antpos_flat = {k: np.array([k * 10, 0, 0]) for k in range(nants)}
-
-    # Define a Gaussian beam
-    beam = AnalyticBeam("gaussian", diameter=14.0)
-
-    # Set sky model
-    ra = np.linspace(0.0, 2.0 * np.pi, nsrcs)
-    dec = np.linspace(-0.5 * np.pi, 0.5 * np.pi, nsrcs)
-    sky_model = rng.uniform(0, 1, size=(nsrcs, 1)) * (freqs[None] / 150e6) ** -2.5
-
-    # Use matvis as a reference
-    mvis = matvis.simulate_vis(
-        antpos, sky_model, ra, dec, freqs, lsts, beams=[beam], precision=2
-    )
-    mvis_flat = matvis.simulate_vis(
-        antpos_flat, sky_model, ra, dec, freqs, lsts, beams=[beam], precision=2
-    )
-
-    # Use fftvis to simulate visibilities
-    fvis = simulate.simulate_vis(
-        antpos, sky_model, ra, dec, freqs, lsts, beam, precision=2, eps=1e-10,
-        baselines=[(i, j) for i in range(nants) for j in range(nants)]
-    )
-    fvis.shape = (nfreqs, ntimes, nants, nants)
-
-    # Check that the results are the same
-    np.testing.assert_allclose(mvis, fvis, atol=1e-5)
-
-    # Check that the results are different
-    assert not np.allclose(mvis_flat, fvis, atol=1e-5)
