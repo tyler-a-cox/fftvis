6bcfaf72242d7204b5188b6dff1e58d61a8cdb60
Steven Murray <steven.g.murray@asu.edu>
2024-10-23 09:33:24 +0200

    perf: better ray environments for tracking memory and shm

--- fftvis/simulate.py
+++ fftvis/simulate.py
@@ -330,6 +330,15 @@ def simulate(
         
         logger.info(f"Initializing with {2*required_shm/1024**3:.2f} GB of shared memory")
         if not ray.is_initialized():
+            if trace_mem:
+                # Record which lines of code assign to shared memory, for debugging.
+                os.environ['RAY_record_ref_creation_sites'] = 1
+                
+            # Only spill shared memory objects to disk if the Store is totally full.
+            # If we don't do this, then since we need a relatively small amount of
+            # SHM, it starts writing to disk even though we never needed to.
+            os.environ['RAY_object_spilling_threshold'] = 1.0
+            
             try:
                 ray.init(
                     num_cpus=nprocesses,
