b24b815502386f067a9f13c1173df04d3455f77d
Steven Murray <steven.g.murray@asu.edu>
2024-11-08 14:09:59 +0100

    maint: update to use BeamInterface API

--- fftvis/tests/test_compare_matvis.py
+++ fftvis/tests/test_compare_matvis.py
@@ -2,8 +2,7 @@ import matvis
 import pytest
 import numpy as np
 from fftvis import simulate
-from pyuvsim.analyticbeam import AnalyticBeam
-from . import get_standard_sim_params
+from matvis._test_utils import get_standard_sim_params
 
 @pytest.mark.parametrize('polarized', [False, True])
 @pytest.mark.parametrize('precision', [2,1])
@@ -12,56 +11,49 @@ from . import get_standard_sim_params
 @pytest.mark.parametrize('nprocesses', [1, 2])
 def test_simulate(polarized: bool, precision: int, use_analytic_beam: bool, tilt_array: bool, nprocesses: int):
 
-    (
-        ants,
-        flux,
-        ra,
-        dec,
-        freqs,
-        lsts,
-        times,
-        cpu_beams,
-        location,
-    ) = get_standard_sim_params(use_analytic_beam, polarized)
-    
+    params, *_ = get_standard_sim_params(
+        use_analytic_beam=use_analytic_beam, polarized=polarized
+    )
+    ants = params.pop('ants')
     if tilt_array:
         # Tilt the array
         ants = {ant: np.array(ants[ant]) + np.array([0, 0, ai * 5]) for ai, ant in enumerate(ants)}
-
+    
     # Simulate with specified baselines
     sim_baselines = np.array([[0, 1]])#, [0, 2], [1, 2]])
 
     # Use matvis as a reference
     mvis = matvis.simulate_vis(
         ants=ants,
-        fluxes=flux,
-        ra=ra,
-        dec=dec,
-        freqs=freqs,
-        times=times,
-        beams=cpu_beams,
-        telescope_loc=location,
-        polarized=polarized,
         precision=precision,
         antpairs=sim_baselines,
         coord_method='CoordinateRotationERFA',
-        source_buffer=0.75
+        source_buffer=0.75,
+        **params
     )
+
+    beam = params.pop("beams")[0]
+    times = params.pop('times').jd
 
     # Use fftvis to simulate visibilities
     fvis = simulate.simulate_vis(
-        ants, flux, ra, dec, freqs, times.jd, cpu_beams[0], eps=1e-10 if precision==2 else 6e-8,
-        baselines=sim_baselines, polarized=polarized, precision=precision,
-        telescope_loc=location, nprocesses=nprocesses,
+        ants, eps=1e-10 if precision==2 else 6e-8,
+        baselines=sim_baselines, precision=precision,
+        nprocesses=nprocesses,
         coord_method_params={"source_buffer": 0.75},
         trace_mem=False,
+        beam=beam,
+        times=times,
+        **params
     )
 
+    freqs = params['freqs']
+    
     # Should have shape (nfreqs, ntimes, nants, nants)
     if polarized:
-        assert fvis.shape == (len(freqs), len(lsts), 2, 2, len(sim_baselines))
+        assert fvis.shape == (len(freqs), len(times), 2, 2, len(sim_baselines))
     else:
-        assert fvis.shape == (len(freqs), len(lsts), len(sim_baselines))
+        assert fvis.shape == (len(freqs), len(times), len(sim_baselines))
 
     # Check that the polarized results are the same
     for bi, bl in enumerate(sim_baselines):
